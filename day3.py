a5b71c3071723cda0a91ad012b347fa9a67dd281
hamerdop <162981056+hamerdop@users.noreply.github.com>
2024-03-15 09:22:32 +0530

    Add files via upload

---
+++ day3.py
@@ -0,0 +1,66 @@
+'''
+#length int(input())
+#breadth int(input())
+#if length breadth: print("its a square") #
+#else:
+# print("its not square")
+#Eg:4
+# Python program to check whether the
+# given integer is a multiple of both 5 and7.
+
+n=int(input("enter the number: "))
+if n%5=0 and n%7==0:
+print("yes")
+else:
+print("no")
+Eg:5
+#Accept the age of 4 people and display the oldest one.
+#road tax to be paid
+#according to the following criteria
+
+#road tax to be paid
+#according to the following criteria:
+#
+Cost price (in Rs )                                         Tax
+# > 100000                                                  15% + discount 6%
+# > 50000 and <= 100000                                     10%
+# <= 50000                                                  5%
+'''
+'''
+price = int(input("Enter the price: "))
+amount= 0
+if price>=100000:
+    discount+ price*(6/100)
+    value = price-discount
+    tax= value (15/100)
+    total= value+tax
+else:
+    tax =price*(5/100)
+    total=price+tax
+print("The onroad cost of bike is: ",total)
+'''
+'''
+# print the output of 7th multiplication table from 21 to 43.
+for val in range=(1, 10+1,):
+#print=('7', 'x', val, '=', val 7) --> method:1
+# Method: 2
+#ans="7 x () {}"
+#print(ans.format(val, val*7))                                      
+'''
+'''
+for val in range(1, 10):
+if val ==6:
+print(val)
+break
+'''
+'''
+#Eg:1
+for val in range(1, 10): ifval = - 6
+continue
+print(val)
+'''
+
+#
+for val in range(1, 10): ifval = - 6
+continue
+print(val)
